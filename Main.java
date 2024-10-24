1ba48a19c75a3c40e22bea701d673cc85342ad5e
Afrancisfinney <142611499+Afrancisfinney@users.noreply.github.com>
2024-10-23 19:31:20 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,38 +1,40 @@
 import java.util.Scanner;
 import java.util.Random;
-public class Main{
 
-    public static void main(String[]args){
+public class Main {
+
+    public static void main(String[] args) {
         Random rand = new Random();
         Scanner scan = new Scanner(System.in);
 
-        int x = rand.nextInt(100);
+        int x = rand.nextInt(101); 
         int guess;
+        int count = 0; 
+
         System.out.println("Enter your first guess:");
         guess = scan.nextInt();
-        int count=0; //count the number of time user guesses
+        count++;
 
-        while(guess!=x){
-        if (guess<x) {
-            System.out.println("Guess is too low,please try again");
+        while (guess != x) {
+            if (guess < x) {
+                System.out.println("Too low!");
+            } else {
+                System.out.println("Too high!");
+            }
 
-        } else if (guess>x) {
-            System.out.println("Guess is too high, please try again");
-        } 
-            
-        System.out.println("Enter your next guess:");
-        guess = scan.nextInt();
-        if (guess==x){
-            System.out.println("You got it");
+            System.out.println("Enter your next guess:");
+            guess = scan.nextInt();
+            count++;
         }
 
-        
+        System.out.println("You got it!");
 
-
-            
+        if (count <= 7) {
+            System.out.println("You got an impossibly good score!");
+        } else if (count <= 10) {
+            System.out.println("You did a good job!");
+        } else {
+            System.out.println("Try the divide and conquer strategy next time!");
         }
-
-
-
     }
 }
